e822f10cfc3071f578527bc72d819883b7ed2b08
fradvocatus <alfriyadi.rafles@binus.ac.id>
2020-12-27 06:17:16 +0700

    Added Store & Product eloquent(model), controller, and factory.

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Model;
+use App\Models\Product;
+use App\Models\Store;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $store = Store::factory()->create();
+        return [
+            'store_id' => Store::latest()->first(),
+            'name' => $this->faker->word,
+            'quantity' => $this->faker->numberBetween($min = 0, $max = 100),
+            'price' => $this->faker->randomFloat($nbMaxDecimals= 2, $min = 0, $max= 999),
+            'description' => $this->faker->text,
+            'image' => NULL
+        ];
+    }
+}
