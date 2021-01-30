1e13f151a4358f6127a757bed0d328b5f3f26327
fradvocatus <alfriyadi.rafles@binus.ac.id>
2021-01-31 01:54:17 +0700

    Refactored ProductController.php

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -10,20 +10,21 @@ class ProductController extends Controller
 {
     public function index()
     {
-        if(session()->has('credentials')){
+        if (session()->has('credentials')) {
             $product = Product::all();
             $store = \App\Models\Store::all();
-            return view('product.index', ['product'=>$product, 'store'=>$store]);
-        }else{
+            return view('product.index', ['product' => $product, 'store' => $store]);
+        } else {
             return redirect('/');
         }
     }
 
-    public function indexStore(string $id){
+    public function indexStore(string $id)
+    {
         $store = Store::find($id);
         return view('stores.products.index', [
             'store' => $store,
-            'activeListing' => $store->products->where('is_active', $id),
+            'activeListing' => $store->products->where('is_active', true),
             'products' => $store->products
         ]);
     }
